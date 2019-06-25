e243030e6e1cbc84e9d48ccb7bce316b0d8accef
Sandro Hawke <sandro@hawke.org>
2019-06-25 12:27:28 -0400

    some random fixes

--- set-me-up.sh
+++ set-me-up.sh
@@ -25,7 +25,16 @@ mkdir -m 755 -p /sites/$HOST
 echo "server set up $(date -Ru)" > /sites/$HOST/index.html
 chmod 644 /sites/$HOST/index.html
 
-# rm /etc/nginx/sites-available/default
+cat <<EOF > /etc/nginx/sites-available/default
+# redirect all traffic for unexpected hostnames to our primary
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	server_name _;
+	return 301 http://$HOST\$request_uri;
+}
+EOF
+
 CONF=/etc/nginx/sites-available/$HOST
 if [ -f $CONF ]; then
     BAK=$CONF-$(date -Is)
@@ -37,3 +46,14 @@ ln -s /etc/nginx/sites-available/$HOST /etc/nginx/sites-enabled/$HOST
 
 nginx -t || exit 1
 service nginx restart && echo 'nginx restarted'
+
+adduser --disabled-password --gecos 'Sandro Hawke' sandro
+mkdir -p ~sandro/.ssh
+cp /root/.ssh/authorized_keys ~sandro/.ssh/authorized_keys
+cd ~sandro
+chown -R sandro .ssh
+chmod 700 .ssh
+chmod 600 .ssh/authorized_keys
+apt-get install sudo
+echo 'sandro ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/sandro
+
