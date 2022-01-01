bf90fd842e478e0bb190fea1b47491e97b182b59
Sandro Hawke <sandro@hawke.org>
2022-01-01 18:11:09 -0500

    scaling

---
+++ 5-webcam-added.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+##sudo chown sandro.sandro /tmp/scripts.log /tmp/printenv
+##echo "USB device removed  at $(date)" >>/tmp/scripts.log
+##printenv >>/tmp/printenv
+notify-send "Camera available"
+# BORKEN! No longer exists.
+# v4l2ctrl -d /dev/video1 -l /home/sandro/camera-settings-140.txt
+v4l2-ctl -d 2 --set-ctrl=zoom_absolute=140
